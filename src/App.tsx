0fd02264db1d7a990d6468117f64ebe472379b90
Nap Joseph Calub <njncalub@gmail.com>
2021-02-09 22:07:48 +0800

    Added font-awesome icons (#14).

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,8 @@
 import { LoginButton, LogoutButton } from "./components";
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { fab } from "@fortawesome/free-brands-svg-icons";
+
+library.add(fab);
 
 interface Props {
   title: string;
