eadd7538f2fe63f4b67fb92f7775709e4efe70e8
Oscar Otero <oom@oscarotero.com>
2025-05-08 11:29:36 +0200

    lume 3

--- plugins.ts
+++ plugins.ts
@@ -1,9 +1,6 @@
-import lightningcss from "lume/plugins/lightningcss.ts";
 import tailwindcss from "lume/plugins/tailwindcss.ts";
 import inline from "lume/plugins/inline.ts";
 import icons from "lume/plugins/icons.ts";
-import typography from "npm:@tailwindcss/typography";
-import postcss from "lume/plugins/postcss.ts";
 import basePath from "lume/plugins/base_path.ts";
 import metas from "lume/plugins/metas.ts";
 import { Options as SitemapOptions, sitemap } from "lume/plugins/sitemap.ts";
@@ -28,18 +25,13 @@ export default function (userOptions?: Options) {
   const options = merge(defaults, userOptions);
 
   return (site: Lume.Site) => {
-    site.use(lightningcss())
-      .use(basePath())
+    site.use(basePath())
       .use(metas())
       .use(icons())
+      .use(tailwindcss())
       .use(inline())
       .use(sitemap(options.sitemap))
       .use(favicon(options.favicon))
-      .use(tailwindcss({
-        options: {
-          plugins: [typography],
-        },
-      }))
-      .use(postcss());
+      .add("style.css");
   };
 }
